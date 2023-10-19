9dd6d52fafb95eb5633a981b07208721e1261bb5
Mukesh-Negi <107361627+Mukesh-Negi@users.noreply.github.com>
2023-10-19 23:25:29 +0530

    FCFS SCHEDULING ALGO.

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,69 +1,87 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void findWait(int wt[], int bt[], int n)
+// Function to Calculate waiting time
+// and average waiting time
+void CalculateWaitingTime(int at[],
+                          int bt[], int N)
 {
+
+    // Declare the array for waiting
+    // time
+    int wt[N];
+
+    // Waiting time for first process
+    // is 0
     wt[0] = 0;
 
-    for (int i = 1; i < n; i++)
+    // Print waiting time process 1
+    cout << "PN\t\tAT\t\t"
+         << "BT\t\tWT\n\n";
+    cout << "1"
+         << "\t\t" << at[0] << "\t\t"
+         << bt[0] << "\t\t" << wt[0] << endl;
+
+    // Calculating waiting time for
+    // each process from the given
+    // formula
+    for (int i = 1; i < 5; i++)
     {
-        wt[i] = bt[i - 1] + wt[i - 1];
-    }
-}
+        wt[i] = (at[i - 1] + bt[i - 1] + wt[i - 1]) - at[i];
 
-void findTat(int tat[], int bt[], int wt[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        tat[i] = bt[i] + wt[i];
-    }
-}
-
-void findAvg(int bt[], int n)
-{
-    int wt[n];
-    int tat[n];
-
-    findWait(wt, bt, n); // calculate wait time
-
-    findTat(tat, bt, wt, n); // calculate turn around time
-
-    float totalWait = 0;
-    float totalTat = 0;
-
-    cout << "P \t"
-         << "BT\t  "
-         << "WT\t"
-         << "TAT\n";
-    for (int i = 0; i < n; i++)
-    {
-        totalWait += wt[i];
-
-        totalTat += tat[i];
-
-        cout << 3
-             << i + 1 << "\t" << bt[i] << "    \t" << wt[i] << "\t" << tat[i] << endl;
+        // Print the waiting time for
+        // each process
+        cout << i + 1 << "\t\t" << at[i]
+             << "\t\t" << bt[i] << "\t\t"
+             << wt[i] << endl;
     }
 
-    cout << "Avg. waiting time :-> " << totalWait / n << endl;
+    // Declare variable to calculate
+    // average
+    float average;
+    float sum = 0;
 
-    cout << "Avg. TURN AROUND TIME time :-> " << totalTat / n;
+    // Loop to calculate sum of all
+    // waiting time
+    for (int i = 0; i < 5; i++)
+    {
+        sum = sum + wt[i];
+    }
+
+    // Find average waiting time
+    // by dividing it by no. of process
+    average = sum / 5;
+
+    // Print Average Waiting Time
+    cout << "\nAverage waiting time = "
+         << average;
 }
 
+// Driver code
 int main()
 {
-    int n;
+    // Number of process
     cout << "ENTER NUMBER OF PROCESS :->";
+    int n;
     cin >> n;
 
-    int bTime[n];
-
-    cout << "ENTER BURST TIME OF THE PROCESS :->";
+    // Array for Arrival time
+    int at[n];
+    int bt[n];
+    cout << "ENTER ARRIVAL TIME :-> \n";
     for (int i = 0; i < n; i++)
     {
-        cin >> bTime[i];
+        cin >> at[i];
     }
 
-    findAvg(bTime, n);
+    cout << "ENTER BURST TIME :-> \n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> bt[i];
+    }
+
+    // Function call to find
+    // waiting time
+    CalculateWaitingTime(at, bt, n);
     return 0;
 }
